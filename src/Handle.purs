f200d52705e29580ce04ae98a8ad9f801a29c858
Patrick Thomson <patrickt@github.com>
2022-11-15 16:12:30 -0500

    Start implementing StackGraph.

--- src/Handle.purs
+++ src/Handle.purs
@@ -1,11 +1,15 @@
-module Handle (Handle (..)) where
+module Handle (Handle, unsafe) where
 
 import Prelude
 
 import Data.UInt (UInt)
+import Data.UInt as UInt
 
 -- TODO: region parameter here?
 newtype Handle (a :: Type) = Handle UInt
 
 derive newtype instance eqHandle :: Eq (Handle a)
 derive newtype instance showHandle :: Show (Handle a)
+
+unsafe :: forall a . Int -> Handle a
+unsafe i = Handle (UInt.fromInt i)
---
+++ src/StackGraph.purs
@@ -0,0 +1,37 @@
+module StackGraph where
+
+import Prelude
+
+import Data.Maybe (Maybe (..))
+import Data.Either (Either (..))
+import Control.Monad.ST (ST)
+import Control.Monad.ST as ST
+
+import Node (Node, Visibility, Nature)
+import NodeID (NodeID)
+import Handle (Handle)
+import Handle as Handle
+import File (File)
+
+newtype StackGraph (r :: ST.Region) = StackGraph { todo :: String }
+
+addScopeNode :: forall r . NodeID -> Visibility -> StackGraph r -> ST r (Maybe (Handle Node))
+addScopeNode _ _ _ = pure Nothing
+
+addPushSymbolNode :: forall r . NodeID -> Handle String -> Nature -> StackGraph r -> ST r (Maybe (Handle Node))
+addPushSymbolNode _ _ _ _ = pure Nothing
+
+addFile :: forall r . String -> StackGraph r -> ST r (Either (Handle File) (Handle File))
+addFile _ _ = pure (Left (Handle.unsafe 666))
+
+getOrCreateFile :: forall r . String -> StackGraph r -> ST r (Handle File)
+getOrCreateFile _ _ = pure (Handle.unsafe 666)
+
+getFile :: forall r . String -> StackGraph r -> ST r (Maybe (Handle File))
+getFile _ _ = pure Nothing
+
+eachNodeInFile :: forall r . Handle File -> StackGraph r -> (Handle Node -> ST r Unit) -> ST r Unit
+eachNodeInFile _ _ _ = pure unit
+
+eachFile :: forall r . StackGraph r -> (Handle File -> ST r Unit) -> ST r Unit
+eachFile _ _ = pure unit
